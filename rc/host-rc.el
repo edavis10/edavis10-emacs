b18fbafdd3fd574882b14e66222eed03afbb48cd
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-23 17:59:08 -0800

    Added drow settings

--- rc/host-rc.el
+++ rc/host-rc.el
@@ -10,9 +10,17 @@
   (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-8"))
   (print "aether settings applied"))
 
+(defun host-drow-settings ()
+  "These are setting specific to drow, my netbook"
+  (set-default-font "Bitstream Vera Sans Mono-10")
+  (add-to-list 'default-frame-alist '(font . "Bitstream Vera Sans Mono-10"))
+  (print "drow settings applied"))
+
 (cond 
  ((equal system-name "acheron.home.theadmin.org")
   (host-acheron-settings))
  ((equal system-name "aether.home.theadmin.org")
   (host-aether-settings))
+ ((equal system-name "drow.home.theadmin.org")
+  (host-drow-settings))
  (t (print system-name)))

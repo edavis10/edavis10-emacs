a8f434a12ada142f024d5919cf8bc93b7600a682
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-17 22:01:33 -0800

    More markdown mode extensions

--- rc/markdown-rc.el
+++ rc/markdown-rc.el
@@ -1,4 +1,6 @@
 ;; Markdown mode
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.mdwn$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.page$" . markdown-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.eml$" . markdown-mode) auto-mode-alist))
 
